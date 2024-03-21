739665670661dba2ea31ab39fca87d7e62396d26
rhagilsetiawan <159235607+rhagilsetiawan@users.noreply.github.com>
2024-03-21 09:21:56 +0700

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        @vite('resources/js/app.js')
+    </body>
+</html>
